4eb64061ce84d6d4ad9ac1ee5ce727c20f6be715
E-v7 <33555329+E-v7@users.noreply.github.com>
2025-03-01 23:46:02 -0500

    Added blueprint to flask app in main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from esg_summary import esg_summary_page
+from scraper import scraper_page
 
 app = Flask(__name__)
 app.register_blueprint(esg_summary_page)
+app.register_blueprint(scraper_page)
 
 app.run(port=5000)
